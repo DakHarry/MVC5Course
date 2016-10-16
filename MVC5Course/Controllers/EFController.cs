fd7d4e945ff4ae549b078fd7138a306be14bd471
Harry.Chan <harry.chan@soohoobook.com>
2016-10-16 16:54:39 +0800

    22 練習先變更資料庫中 Product.IsDeleted 的欄位名稱，再去更新 EDMX 實體資料模型，並確保程式可正常執行 23 練習 Entity Framework 設計工具對實體的各種操作 24 練習匯入資料庫中的 vw_ClientContribution 檢視表，並顯示資料在畫面上 25 練習對 Entity Framework 的資料存取程式進行效能調校 26 練習使用 Entity Framework 呼叫 Stored Procedure 預存程序以取得資料

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -1,4 +1,5 @@
 ﻿using MVC5Course.Models;
+using MVC5Course.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity.Validation;
@@ -79,18 +80,70 @@ namespace MVC5Course.Controllers
         }
         public ActionResult Update20percent()
         {
-            var product = db.Product.Where(p => p.ProductName.Contains("White"));
-            foreach (var item in product)
-            {
-                if (item.Price.HasValue)
-                {
-                    item.Price = item.Price.Value * 1.2m;
-                }
+            string str = "%White%";
+            db.Database.ExecuteSqlCommand("UPDATE dbo.Product SET Price = Price*1.2 WHERE ProductName LIKE @p0",str);
+            //var product = db.Product.Where(p => p.ProductName.Contains("White"));
+            //foreach (var item in product)
+            //{
+            //    if (item.Price.HasValue)
+            //    {
+            //        item.Price = item.Price.Value * 1.2m;
+            //    }
                 
-            }
-            db.SaveChanges();
+            //}
+            //db.SaveChanges();
             return RedirectToAction("Index");
 
         }
+        public ActionResult ClientContribution()
+        {
+            //ViewModal方式顯示ClientContribution資料,新增View不需要選擇"資料內容類別"，不隸屬任何資料庫
+            var data = db.vw_ClientContribution.Take(10);
+            return View(data);
+        }
+        //執行原始查詢 (無回傳型別 ) (DDL/DML))
+        /*
+        public ActionResult ClientContribution2(string keyword = "Mary")
+        {
+            var data = db.Database.SqlQuery<vw_ClientContribution>(@"
+        SELECT
+   c.ClientId,
+   c.FirstName,
+   c.LastName, 
+         (SELECT  SUM(o.OrderTotal) From [dbo].[Order] o WHERE o.ClientId = c.ClientId) as OrderTotal
+
+              FROM 
+                     [dbo].[Client] as c 
+                 WHERE
+                        c.FirstName LIKE @p0", "%" + keyword + "%");
+            return View(data);
+        }
+*/
+
+        // 建立原始查詢(帶有回傳型別 ) (SELECT/SP)) 
+        public ActionResult ClientContribution2(string keyword = "Mary")
+        {
+
+            var data = db.Database.SqlQuery<ClientContributionViewModel>(@"
+        SELECT
+			c.ClientId,
+			c.FirstName,
+			c.LastName, 
+	        (SELECT  SUM(o.OrderTotal) From [dbo].[Order] o WHERE o.ClientId = c.ClientId) as OrderTotal
+
+	    FROM 
+	           [dbo].[Client] as c 
+        WHERE
+               c.FirstName LIKE @p0", "%" + keyword + "%");
+            return View(data);
+        }
+        public ActionResult ClientContribution3(string keyword="Mary")
+        {
+            //回傳型別:ObjectResult    Stored Procedure: 新增View不需要選擇"資料內容類別",EF預設不支援,所以將其視為ViewModel
+            var data = db.usp_GetClientContribution(keyword);
+            return View(data);
+        }
+
+
     }
 }
