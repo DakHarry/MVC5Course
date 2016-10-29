3e830dd80a5e69a34f9c6aa0622381e95909a5f6
Harry.Chan <harry.chan@soohoobook.com>
2016-10-29 13:48:34 +0800

    40 練習 FileResult 的使用，顯示或下載一個圖檔  41 練習 JsonResult 的使用

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC5Course.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,37 @@ namespace MVC5Course.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult PartialViewTest()
+        {
+            return PartialView();
+        }
+
+        public ActionResult FileTest()
+        {
+            //C:\Users\USER\Desktop\MVC5Course\MVC5Course\Content\GodDameCat.jpg
+            var filepath = Server.MapPath("~/Content/GodDameCat.jpg");
+            return File(filepath,"image/jpeg");
+        }
+
+
+        public ActionResult FileTest2()
+        {
+            //C:\Users\USER\Desktop\MVC5Course\MVC5Course\Content\GodDameCat.jpg
+            var filepath = Server.MapPath("~/Content/GodDameCat.jpg");
+            return File(filepath, "image/jpeg","GodDameCat.jpg");
+        }
+
+        public ActionResult JSonTest()
+        {
+            ProductRepository repo = RepositoryHelper.GetProductRepository();
+           
+            // db.Configuration.LazyLoadingEnabled = false;
+            repo.UnitOfWork.LazyLoadingEnabled = false;
+            var data =repo.All().Take(50);
+           
+            return Json(data,JsonRequestBehavior.AllowGet);
         }
     }
 }
