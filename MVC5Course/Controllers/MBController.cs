f9434f32482a08bb7e17c1548423e52f0416781c
Harry.Chan <harry.chan@soohoobook.com>
2016-10-29 16:10:05 +0800

    43 練習複雜模型繫結並可一次傳入多筆複雜模型資料繫結到陣列裡

--- MVC5Course/Controllers/MBController.cs
+++ MVC5Course/Controllers/MBController.cs
@@ -1,4 +1,5 @@
-﻿using MVC5Course.Models.ViewModels;
+﻿using MVC5Course.Models;
+using MVC5Course.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace MVC5Course.Controllers
 {
     public class MBController : Controller
     {
+        FabricsEntities db = new FabricsEntities();
         // GET: MB
         public ActionResult Index()
         {
@@ -17,7 +19,7 @@ namespace MVC5Course.Controllers
             {
                 FirstName = "Harry",
                 LastName = "Chan",
-                Gender="Man",
+                Gender="M",
                 MiddleName="DK"
                    
             };
@@ -45,6 +47,39 @@ namespace MVC5Course.Controllers
 
         public ActionResult MyFormResult()
         {
+            return View();
+        }
+
+
+        public ActionResult ProductList()
+        {
+
+            var data = db.Product.Take(20).ToList();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult BatchUpdate(ProductBatchUpdateViewModel[] items)
+        {
+            /*
+             * item.ProductId
+             * item[0].ProductId
+             * 
+             * */
+            if (ModelState.IsValid)
+            {
+                foreach (var item in items)
+                {
+                    var product = db.Product.Find(item.ProductId);
+                    product.ProductName = item.ProductName;
+                    product.Active = item.Active;
+                    product.Price = item.Price;
+                    product.Stock = item.Stock;
+                }
+                db.SaveChanges();
+                return RedirectToAction("ProductList");
+            }
+
             return View();
         }
     }
