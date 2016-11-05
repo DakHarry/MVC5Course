43d6e45be4b2d0da264f7763f82b60bdef82ff42
Harry.Chan <harry.chan@soohoobook.com>
2016-11-05 13:58:32 +0800

    61 練習 Ajax.JavaScriptStringEncode 的用法  62 練習開發 ASP.NET Web API 控制器，並用 Postman 進行測試 ( /api/ProductsApi )  63 使用 Postman 對 ASP.NET Web API 進行操作 ( POST, GET, PUT, DELETE )

---
+++ MVC5Course/Controllers/ProductsApi1Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVC5Course.Controllers
+{
+    public class ProductsApi1Controller : ApiController
+    {
+        // GET: api/ProductsApi1
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ProductsApi1/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ProductsApi1
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ProductsApi1/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ProductsApi1/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MVC5Course/Controllers/ProductsApiController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Routing;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    /*
+    WebApiConfig 類別可能需要其他變更以新增此控制器的路由，請將這些陳述式合併到 WebApiConfig 類別的 Register 方法。注意 OData URL 有區分大小寫。
+
+    using System.Web.Http.OData.Builder;
+    using System.Web.Http.OData.Extensions;
+    using MVC5Course.Models;
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Product>("ProductsApi");
+    builder.EntitySet<OrderLine>("OrderLine"); 
+    config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+    */
+    public class ProductsApiController : ODataController
+    {
+        private FabricsEntities db = new FabricsEntities();
+
+        public ProductsApiController()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+        }
+        // GET: odata/ProductsApi
+        [EnableQuery]
+        public IQueryable<Product> GetProductsApi()
+        {
+            return db.Product;
+        }
+
+        // GET: odata/ProductsApi(5)
+        [EnableQuery]
+        public SingleResult<Product> GetProduct([FromODataUri] int key)
+        {
+            return SingleResult.Create(db.Product.Where(product => product.ProductId == key));
+        }
+
+        // PUT: odata/ProductsApi(5)
+        public IHttpActionResult Put([FromODataUri] int key, Delta<Product> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Product product = db.Product.Find(key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(product);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(product);
+        }
+
+        // POST: odata/ProductsApi
+        public IHttpActionResult Post(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Product.Add(product);
+            db.SaveChanges();
+
+            return Created(product);
+        }
+
+        // PATCH: odata/ProductsApi(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public IHttpActionResult Patch([FromODataUri] int key, Delta<Product> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Product product = db.Product.Find(key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(product);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(product);
+        }
+
+        // DELETE: odata/ProductsApi(5)
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            Product product = db.Product.Find(key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            db.Product.Remove(product);
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // GET: odata/ProductsApi(5)/OrderLine
+        [EnableQuery]
+        public IQueryable<OrderLine> GetOrderLine([FromODataUri] int key)
+        {
+            return db.Product.Where(m => m.ProductId == key).SelectMany(m => m.OrderLine);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductExists(int key)
+        {
+            return db.Product.Count(e => e.ProductId == key) > 0;
+        }
+    }
+}
